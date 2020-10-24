b8271e4efd74918a519bcf4cc39e3131203c4656
MalekMK <mkaouarmalek@gmail.com>
2020-10-24 11:18:46 +0100

    adding the frontend part

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import HomePage from "./Components/HomePage";
+import ErrorPage from "./Components/ErrorPage";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route component={ErrorPage} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class HompePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>404 - Not Found!</h1>
+      </div>
+    );
+  }
+}
