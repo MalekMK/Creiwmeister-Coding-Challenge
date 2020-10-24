bc2a5d2fc6880b0220ea23bfb0770f3a2a4912d7
MalekMK <mkaouarmalek@gmail.com>
2020-10-24 01:57:03 +0100

    changing the method of handling requests

--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,25 @@
-import express from 'express';
-import { FilterAbsencesList, FilterAbsencesListById, FilterAbsencesListByDate } from './filters.js';
+import express from "express";
+import {
+  FilterAbsencesList,
+  FilterAbsencesListById,
+  FilterAbsencesListByDate,
+} from "./filters.js";
 
 const routes = express.Router();
 
-//rendering all the absenses
-routes.get('/', async (request, response) => {
-    const absences = await FilterAbsencesList();
-    return response.json(absences);   
-});
-
-//rendering the absenses of a specific userId
-routes.get('/id/:userId', async (request, response) => {
-    const userId = Number(request.params.userId);
+routes.get("/", async (request, response) => {
+  const userId = Number(request.query.userId);
+  const startDate = request.query.startDate;
+  const endDate = request.query.endDate;
+  if (userId) {
     const filterUser = await FilterAbsencesListById(userId);
     return response.json(filterUser);
-});
-
-//rendering the absenses of users between 2 dates
-routes.get('/from/:startDate/to/:endDate', async (request, response) => {
-    const startDate = request.params.startDate;
-    const endDate = request.params.endDate;
-    const filterAbs = await FilterAbsencesListByDate(startDate,endDate);
+  } else if (startDate || endDate) {
+    const filterAbs = await FilterAbsencesListByDate(startDate, endDate);
     return response.json(filterAbs);
+  } else {
+    const absences = await FilterAbsencesList();
+    return response.json(absences);
+  }
 });
-
 export default routes;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import routes from "./routes.js"
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
 app.use(routes);
 app.listen(3001, () =>  console.log('Server Started!')) // starting the server
