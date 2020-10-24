d585ed972622b65d87ad677bc8542da09519b146
MalekMK <mkaouarmalek@gmail.com>
2020-10-24 01:59:42 +0100

    adding some comments

--- src/routes.js
+++ src/routes.js
@@ -11,13 +11,13 @@ routes.get("/", async (request, response) => {
   const userId = Number(request.query.userId);
   const startDate = request.query.startDate;
   const endDate = request.query.endDate;
-  if (userId) {
+  if (userId) { // if we have an userId parameter
     const filterUser = await FilterAbsencesListById(userId);
     return response.json(filterUser);
-  } else if (startDate || endDate) {
+  } else if (startDate || endDate) { // if we have startDate and/or endDate parameter
     const filterAbs = await FilterAbsencesListByDate(startDate, endDate);
     return response.json(filterAbs);
-  } else {
+  } else { // if we don't have any parameter
     const absences = await FilterAbsencesList();
     return response.json(absences);
   }
