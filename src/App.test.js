cdb6cd818eb84cfce368b4477e2036d80b7f08f0
MalekMK <mkaouarmalek@gmail.com>
2020-10-25 15:16:32 +0100

    updating the test script

--- src/App.test.js
+++ src/App.test.js
@@ -2,18 +2,36 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import ErrorPage from './Components/ErrorPage';
 import Header from "./Components/Header";
-import App from "./App"
-import { shallow } from 'enzyme';
+import Enzyme ,{ shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import renderer from "react-test-renderer";
+import AppBar from "@material-ui/core/AppBar";
 
+Enzyme.configure({ adapter: new Adapter() })
 
-test('Error page renders 404', () => {
+describe('Error page should contain 404', () => {
   const { getByText } = render(<ErrorPage/>);
-  const linkElement = getByText(/404 - Not Found!/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(getByText(/404/)).toBeInTheDocument();
 });
 
-test('Header contains welcome and crewmeister', () => {
-  const { getByText } = render(<Header/>);
-  const linkElement = getByText(/Welcome To Crewmeister Absense Manager App/i);
-  expect(linkElement).toBeInTheDocument();
+const setup = (props = {}, state = null) => {
+  const defaultProps = {};
+  const setUpProps = {...defaultProps, ...props};
+  const wrapper = shallow(<Header {...setUpProps}/>);
+  const tree = () => renderer.create(<Header {...setUpProps}/>).toJSON();
+  if (state) wrapper.setState(state);
+  return {wrapper, props: setUpProps, tree};
+};
+
+describe("Header component", () => {
+  it('should match with snapshot', () => {
+      const {tree} = setup();
+      expect(tree()).toMatchSnapshot();
+  });
+
+  it('Should contain Bar', () => {
+      const {wrapper} = setup();
+      expect(wrapper.find(AppBar)).toHaveLength(1);
+  });
+
 });
